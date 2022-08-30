57dc3b3a93e98a67192d2e1765e6b2be298ef718
pashikantirakes <94496861+pashikantirakes@users.noreply.github.com>
2022-08-30 14:39:02 +0530

    Add files via upload

---
+++ src/in/ArrayEquals.java
@@ -0,0 +1,13 @@
+  package in.array.com;    
+  import java.util.Arrays;
+public class ArrayEquals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a1= {1,2,3,4,5};
+		int[] a2= {1,2,3,4,5};
+		System.out.println(Arrays.equals(a1, a2));
+
+	}
+
+}
